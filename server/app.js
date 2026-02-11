8a45db185f0247d15b3371baa485ba83a97d4997
riteshk-007 <codeshorts007@gmail.com>
2026-02-11 16:17:06 +0530

    feat: Initialize the core Express application server with essential middleware, API routes, and error handling.

--- server/app.js
+++ server/app.js
@@ -43,8 +43,8 @@ const app = express();
 
 /* -------------------- BASIC MIDDLEWARE -------------------- */
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: false, limit: '10mb' }));
 app.use(cookieParser());
 
 /* -------------------- REQUEST LOGGER -------------------- */
